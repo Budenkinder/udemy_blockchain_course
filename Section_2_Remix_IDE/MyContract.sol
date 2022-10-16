69337bcbc1272e020cdc8103baa04ffd3b1a53d5
Budenkinder <resul.kocyigit@protonmail.com>
2022-10-16 17:19:26 +0200

    rename and remove comments

--- Section_2_Remix_IDE/MyContract.sol
+++ Section_2_Remix_IDE/MyContract.sol
@@ -6,8 +6,6 @@ contract MyContract{
 
     string public ourString = "Hello World";
 
-    // public for outside world for interaction
-    // no return value
     function updateOurString(string memory _updateString) public {
         ourString = _updateString;
     }
