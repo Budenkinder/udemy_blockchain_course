0474408f3248ec5ce5aeee15ce5d5c61054010ae
Budenkinder <resul.kocyigit@protonmail.com>
2022-10-16 17:17:42 +0200

    Shows the difference of send and transfer when running out of gas or not

--- Section_5_Smart_Wallet/Sample_Low_level_transactions.sol
+++ Section_5_Smart_Wallet/Sample_Low_level_transactions.sol
@@ -6,16 +6,20 @@ pragma solidity 0.8.17;
 contract Sender{
     receive() external payable{}
 
+    //will throw an error, when out of gas
     function withdrawTransfer(address payable _to) public{
         _to.transfer(10);
     }
 
+    //will NOT throw an error, when out of gas
+    //no hint for the user
     function withdrawSend(address payable _to) public{
         _to.send(10);
     }
 }
 
 contract ReceiverNoAction{
+    //has no additional costs
     function balance() public view returns(uint){
         return address(this).balance;
     }
@@ -24,6 +28,7 @@ contract ReceiverNoAction{
 }
 
 contract ReceiverAction{
+    //has additional costs because, storage member is edited
     uint public balanceReceived;
 
     receive() external payable{
