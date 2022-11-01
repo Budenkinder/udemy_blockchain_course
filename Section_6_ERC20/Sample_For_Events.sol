c48324acc05504394e8c22ddadbc6b137ffdc498
Remix Workspace <>
2022-11-01 21:41:47 +0100

    successfull catching event via web3 with this code

--- Section_6_ERC20/Sample_For_Events.sol
+++ Section_6_ERC20/Sample_For_Events.sol
@@ -16,6 +16,9 @@ pragma solidity 0.8.17;
     => The content of the java script file + html will be added
        intot he script folder
     => As javascript editor e.x use A Pen
+
+    This is the result, when catching the event:
+    {"address":"0xE82ED8afCD40b93f2B53ae3222753AE7D0B7e082","blockHash":"0xfa4c6ba58e193daeaca0fdf9d2446cf971ca1a23ee15be770dce27f8923fe852","blockNumber":7874000,"logIndex":36,"removed":false,"transactionHash":"0x84052686b1165f38c30c3725331ba4f6145be6715fc45743b294fa5a360c2e78","transactionIndex":30,"id":"log_12d82ec5","returnValues":{"0":"0x2c2f4d8EE1976B4f4fDC46B4B8fCb6E9A60A88d6","1":"0xE82ED8afCD40b93f2B53ae3222753AE7D0B7e082","2":"10","_from":"0x2c2f4d8EE1976B4f4fDC46B4B8fCb6E9A60A88d6","_to":"0xE82ED8afCD40b93f2B53ae3222753AE7D0B7e082","_amount":"10"},"event":"TokensSent","signature":"0xe607861baff3d292b19188affe88c1a72bdcb69d3015f18bb2cd0bf5349cc3e1","raw":{"data":"0x0000000000000000000000002c2f4d8ee1976b4f4fdc46b4b8fcb6e9a60a88d6000000000000000000000000e82ed8afcd40b93f2b53ae3222753ae7d0b7e082000000000000000000000000000000000000000000000000000000000000000a","topics":["0xe607861baff3d292b19188affe88c1a72bdcb69d3015f18bb2cd0bf5349cc3e1"]}}
 */
 contract EventSample{
 
@@ -27,15 +30,19 @@ contract EventSample{
         tokenBalance[msg.sender] = 100;
     }
 
-    receive() external payable{ }
+    function decreaseTokenBalance(address ofAddress, uint decreaseBy) internal {
+        tokenBalance[ofAddress] -= decreaseBy;        
+    }
+
+    function increaseTokenBalance(address ofAddress, uint increaseBy) internal {
+        tokenBalance[ofAddress] += increaseBy;
+    }
 
     function sendToken(address _to, uint _amount) public returns(bool){
         require(tokenBalance[msg.sender] >= _amount, "Not enough tokens");
-        tokenBalance[msg.sender] -= _amount;
-        tokenBalance[_to] += _amount;
-
+        decreaseTokenBalance(msg.sender,_amount);
+        increaseTokenBalance(_to, _amount);
         emit TokensSent(msg.sender, _to, _amount); 
-
         return true;
     }
 
